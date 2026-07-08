26a5cabff6b4426baba72f3d8050d1edfe99f04c
achmdyuz <achm4d.bat@gmail.com>
2026-07-09 04:19:47 +0700

    MewTwo, Mew, Celebi at Route 29

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1237,27 +1237,27 @@ JohtoGrassWildMons:
 	def_grass_wildmons ROUTE_29
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 2, PIDGEY
+	db 2, CELEBI
 	db 2, SENTRET
-	db 3, PIDGEY
+	db 3, CELEBI
 	db 3, SENTRET
-	db 2, RATTATA
-	db 3, HOPPIP
-	db 3, HOPPIP
+	db 2, MEWTWO
+	db 3, MEW
+	db 3, MEW
 	; day
-	db 2, PIDGEY
+	db 2, CELEBI
 	db 2, SENTRET
-	db 3, PIDGEY
+	db 3, CELEBI
 	db 3, SENTRET
-	db 2, RATTATA
-	db 3, HOPPIP
-	db 3, HOPPIP
+	db 2, MEWTWO
+	db 3, MEW
+	db 3, MEW
 	; nite
 	db 2, HOOTHOOT
-	db 2, RATTATA
+	db 2, MEWTWO
 	db 3, HOOTHOOT
-	db 3, RATTATA
-	db 2, RATTATA
+	db 3, MEWTWO
+	db 2, MEWTWO
 	db 3, HOOTHOOT
 	db 3, HOOTHOOT
 	end_grass_wildmons
